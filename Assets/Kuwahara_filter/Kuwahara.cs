18a4ba701ccb5042073a2d50bf2c5d38016f918a
QuanTrLe <atl226@lehigh.edu>
2026-03-09 22:44:18 -0400

    Commented out the Kuwahara.cs

--- Assets/Kuwahara_filter/Kuwahara.cs
+++ Assets/Kuwahara_filter/Kuwahara.cs
@@ -8,12 +8,12 @@ using UnityEngine;
 public class Kuwahara : MonoBehaviour {
     public Shader kuwaharaShader;
     
-    [Range(1, 20)]
+    [Range(1, 20)] // kernel size to use in filter, if animated then this will be the stopping poine
     public int kernelSize = 1;
 
     public bool animateKernelSize = false;
 
-    [Range(1, 20)]
+    [Range(1, 20)] // starting kernel size when animating
     public int minKernelSize = 1;
 
     [Range(0.1f, 5.0f)]
@@ -22,7 +22,7 @@ public class Kuwahara : MonoBehaviour {
     [Range(0.0f, 30.0f)]
     public float noiseFrequency = 10.0f;
 
-    public bool animateKernelOrigin = false;
+    public bool animateKernelOrigin = false; // as far as i know, doesnt do anything
     
     [Range(1, 4)]
     public int passes = 1;
@@ -30,11 +30,12 @@ public class Kuwahara : MonoBehaviour {
     private Material kuwaharaMat;
     
     void OnEnable() {
-        kuwaharaMat = new Material(kuwaharaShader);
+        kuwaharaMat = new Material(kuwaharaShader); // make a new temp material, sicne this is mainly testing
         kuwaharaMat.hideFlags = HideFlags.HideAndDontSave;
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        // set all the variables of the shader we've had above from the editor 
         kuwaharaMat.SetInt("_KernelSize", kernelSize);
         kuwaharaMat.SetInt("_MinKernelSize", minKernelSize);
         kuwaharaMat.SetInt("_AnimateSize", animateKernelSize ? 1 : 0);
@@ -42,6 +43,7 @@ public class Kuwahara : MonoBehaviour {
         kuwaharaMat.SetFloat("_NoiseFrequency", noiseFrequency);
         kuwaharaMat.SetInt("_AnimateOrigin", animateKernelOrigin ? 1 : 0);
 
+        // make a new rendertexture for how many passes we will have
         RenderTexture[] kuwaharaPasses = new RenderTexture[passes];
 
         for (int i = 0; i < passes; ++i) {
@@ -51,11 +53,13 @@ public class Kuwahara : MonoBehaviour {
         Graphics.Blit(source, kuwaharaPasses[0], kuwaharaMat);
 
         for (int i = 1; i < passes; ++i) {
+            // give the next pass the last pass's info, Blit copies texture
             Graphics.Blit(kuwaharaPasses[i - 1], kuwaharaPasses[i], kuwaharaMat);
         }
 
         Graphics.Blit(kuwaharaPasses[passes - 1], destination);
         for (int i = 0; i < passes; ++i) {
+            // temporary in case we reuse the texture again 
             RenderTexture.ReleaseTemporary(kuwaharaPasses[i]);
         }
     }
